60b6eff4828961f2bb988aa13cb7d415d5b916ed
dreamLogicc <gondin02@mail.ru>
2024-05-07 18:18:32 +0700

    add sorting

--- src/anime/router.py
+++ src/anime/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, desc, asc
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.anime.models import anime
@@ -13,8 +13,26 @@ router = APIRouter(
 
 
 @router.get('/get_anime_list')
-async def get_anime_list(offset: int, session: AsyncSession = Depends(get_async_session)):
-    query = select(anime).offset(offset).limit(10)
+async def get_anime_list(page: int, list_size: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(anime).offset((page - 1) * list_size).limit(list_size)
+    result = await session.execute(query)
+    return [dict(r._mapping) for r in result]
+
+
+@router.get('/ordered_anime_list_by_alphabet')
+async def get_in_alphabet_order(page: int, list_size: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(anime).order_by(anime.c.title_rus).offset((page - 1) * list_size).limit(list_size)
+    result = await session.execute(query)
+    return [dict(r._mapping) for r in result]
+
+
+@router.get('/ordered_anime_list_by_rating')
+async def ordered_anime_list_by_rating(order_type: int, page: int, list_size: int,
+                                       session: AsyncSession = Depends(get_async_session)):
+    if not order_type:
+        query = select(anime).order_by(desc(anime.c.rating)).offset((page - 1) * list_size).limit(list_size)
+    else:
+        query = select(anime).order_by(asc(anime.c.rating)).offset((page - 1) * list_size).limit(list_size)
     result = await session.execute(query)
     return [dict(r._mapping) for r in result]
 
@@ -24,6 +42,7 @@ async def get_anime_info(idx: int, session: AsyncSession = Depends(get_async_ses
     query = select(anime).where(anime.c.id == idx)
     result = await session.execute(query)
     return [dict(r._mapping) for r in result]
+
 
 @router.post('/add_anime')
 async def add_anime(anime_info: AnimeAdd, session: AsyncSession = Depends(get_async_session)):
